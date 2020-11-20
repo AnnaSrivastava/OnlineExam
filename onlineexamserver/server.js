7e20f6a8d096cb6001f44bbe59b766ebc8b1304c
anushka-s <48431681+anushka-s@users.noreply.github.com>
2020-11-20 16:28:11 +0530

    calc score

--- onlineexamserver/server.js
+++ onlineexamserver/server.js
@@ -18,9 +18,9 @@ app.listen(
 );
 app.post("/wordcount", (req, res) => {
 	const {answer} = req.body;
-	let wordsList = [], ignoreList=["the","a","an","and",
-	"it","his","with","to",
-	"of","is","was","","by","then","them","he","she"];
+	let wordsList = [], ignoreList=["the","a","an","and","it","his","with","to","of","is","was","","by","then","them","he","she","may"];
+	let keywordsList = [];
+	let score=0;
 	let j = 0;
 	for (let i = 0; i <= answer.length; i++) {
 	 	if (answer.charAt(i) == " " 
@@ -33,9 +33,25 @@ app.post("/wordcount", (req, res) => {
 	 		j=i+1;
 	 	}
 	 }
+	 
+	 let count=0,score=0,maxScore=10;
+
+     for(let i = 0; i < keywordsList.length ; i++) {
+        for(let j = 0; j < wordsList.length ; j++) {
+            if(keywordsList[i].toLowerCase() === wordsList[j].toLowerCase())
+            {
+                count++;
+                break;
+            }
+        }
+	 }
+	 
+     score=(count/keywordsList.length)*maxScore;
+	 
 	return res.status(200).json(wordsList);
 });
 //logging
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
+
