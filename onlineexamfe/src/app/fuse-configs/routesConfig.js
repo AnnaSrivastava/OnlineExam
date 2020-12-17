932d4ae837f4e27b54adf473f9071ec86df13948
AnnaSrivastava <ananyavsrivastava@gmail.com>
2020-12-17 10:17:25 +0530

    added login page

--- onlineexamfe/src/app/fuse-configs/routesConfig.js
+++ onlineexamfe/src/app/fuse-configs/routesConfig.js
@@ -4,14 +4,15 @@ import FuseUtils from '@fuse/utils';
 import StudentConfig from 'app/main/student/StudentConfig';
 import TeacherConfig from 'app/main/teacher/TeacherConfig';
 import ListConfig from 'app/main/lists/ListConfig';
-import questionBankConfig from 'app/main/questionBank/QuestionBankConfig'
-const routeConfigs = [StudentConfig, TeacherConfig, ListConfig, questionBankConfig];
+import questionBankConfig from 'app/main/questionBank/QuestionBankConfig';
+import LoginConfig from 'app/main/login/LoginConfig';
+const routeConfigs = [StudentConfig, LoginConfig, TeacherConfig, ListConfig, questionBankConfig];
 
 const routes = [
 	...FuseUtils.generateRoutesFromConfigs(routeConfigs),
 	{
 		path: '/',
-		component: () => <Redirect to="/createQuestion" />
+		component: () => <Redirect to="/login" />
 	}
 ];
 
