25bb2e34ce13960c5e317bbb5dea249ed2369649
anushka-s <48431681+anushka-s@users.noreply.github.com>
2021-01-04 17:18:43 +0530

    added mongo atlas connection string

--- onlineexamserver/server.js
+++ onlineexamserver/server.js
@@ -3,6 +3,26 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyParser = require('body-parser');
 const cors =require('cors');
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://Anushka:anushkaonlineexam@onlineexamcluster.a5kyj.mongodb.net/<dbname>?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 //load config
 dotenv.config({ path: "./config/config.env" });
